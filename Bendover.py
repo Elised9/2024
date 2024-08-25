8b35d755008be9b33d2a9a604c277a296c9a63f0
Elised9 <104729502+Elised9@users.noreply.github.com>
2024-08-25 16:38:21 -0600

    Create Bendover.py

---
+++ Bendover.py
@@ -0,0 +1,15 @@
+from PortMap import*
+
+driveBase.straight(230)
+driveBase.turn(-70)
+driveBase.straight(325)
+driveBase.straight(-230)
+driveBase.turn(-20)
+driveBase.straight(400)
+driveBase.turn(80)
+driveBase.straight(200)
+driveBase.turn(-40)
+driveBase.turn(-9)
+driveBase.straight(350)
+
+
